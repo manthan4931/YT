1fdc2df19c74141030a73727ef13ee5b13a5e6ee
manthan4931 <prajapatimanthan49@gmail.com>
2026-04-06 17:51:47 +0530

    final2

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI , HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
 from downloader import download_mp3
 from utils import get_filename
 
@@ -15,14 +15,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 @app.get("/")
 def home():
-    return {"message":"Welcome to the YouTube MP3 Downloader API!"}
+    return FileResponse(os.path.join(BASE_DIR,"docs.html"))
 
 @app.get("/download")
 def download(url:str):
     try:
         file_path=download_mp3(url)
-        return FileResponse(file_path,media_type="audio/mpeg",filename=get_filename(file_path))
+        return FileResponse(file_path,media_type="audio/mpeg",filename=get_filename(file_path),headers={"Content-Disposition": f"attachment; filename={get_filename(file_path)}"}),
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
